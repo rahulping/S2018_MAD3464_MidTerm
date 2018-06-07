c0071455b475931e9344d0340f6a0589e32b7deb
mohit <mohit_nitd@outlook.com>
2018-06-07 19:47:49 -0400

    class decimal

--- 737185_Mad3464_midterm/src/BinaryDecimal.java
+++ 737185_Mad3464_midterm/src/BinaryDecimal.java
@@ -1,13 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Anaisha
- */
 public class BinaryDecimal {
-    
+        public static int convert(String input) {
+        Long binary =  Long.valueOf(input).longValue();
+        int decimalNumber = 0, i = 0;
+        long remainder;
+        while (binary != 0)
+                    {
+            remainder = binary % 10;
+            binary /= 10;
+            decimalNumber += remainder * Math.pow(2, i);
+            ++i;
+        }
+        return decimalNumber;
+    }
+
 }
